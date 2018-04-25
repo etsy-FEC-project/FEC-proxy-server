da314dbf91ca3cbdf8fa1914075344f8d0dcb0a9
Sam Liebow <sam.liebow@gmail.com>
2018-04-25 10:56:51 -0700

    Direct all requests for orderSidebar assets along same GET path

--- server/index.js
+++ server/index.js
@@ -8,21 +8,8 @@ const app = express();
 const port = process.env.PORT || 9000;
 
 app.use(morgan('dev'));
-// matches any valid listing path
-app.use(/listings\/[0-9]{9}\/[A-z-]+\/?(?!orderSidebar)/, express.static(path.join(__dirname, '../public')));
-
-// * matches any amount of text
-app.get('/orderSidebar/:file', (req, res) => {
-  axios.get(`http://127.0.0.1:1541${req.path}`)
-  .then(({headers, data}) => {
-    res.setHeader('content-type', headers['content-type']);
-    res.send(data);
-  })
-  .catch(err => {
-    //console.error(err);
-    res.status(500).send(`Error: ${err}`);
-  });
-});
+// matches any valid URL, not ending with orderSidebar
+app.use(/listings\/[0-9]{9}\/[A-z-]+\/?(?!.*orderSidebar)/, express.static(path.join(__dirname, '../public')));
 
 app.get('/listings/:listingNum/:listingName/orderSidebar/:file', (req, res) => {
   axios.get(`http://127.0.0.1:1541${req.path}`, {responseType: 'arraybuffer'})
@@ -34,7 +21,6 @@ app.get('/listings/:listingNum/:listingName/orderSidebar/:file', (req, res) => {
     console.error(err);
     res.status(500).send(`Error: ${err}`);
   });
-  // http.get(`http://127.0.0.1:1541${req.path}`, (image) => image.pipe(res));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
